70af54fd4a3eefe521cc4a57c0397a4841cc7acb
Logan Bernard <bernard.logan4@gmail.com>
2014-07-28 13:05:48 -0400

    directory existence checks

--- massDOT/travelFeed.py
+++ massDOT/travelFeed.py
@@ -4,9 +4,12 @@ import os
 
 """
 Pulls the xml file for Travel Times from massDOT's website
-and writes it to a file, titled by it's timestamp.
+and writes it to a new file, titled by timestamp.
 
 Creates a new directory, titled by date, to store each day's data.
+
+All new directories and files are stored in a directory 'data'
+inside the current working directory.
 
 ** currently pulling Last-Modified for timestamp from XML data - not response header 
 """
@@ -14,12 +17,14 @@ Creates a new directory, titled by date, to store each day's data.
 request = urllib2.Request('http://www.massdot.state.ma.us/feeds/traveltimes/RTTM_feed.aspx')
 data = urllib2.urlopen(request).read()
 root = ET.fromstring(data)
-
 lastUpdated = root[0].find('LastUpdated').text
 currentDay = lastUpdated[:-13]
-path = os.getcwd() +  '/data/'
 
-if currentDay not in os.listdir(path):
-	os.mkdir(path + currentDay)
-os.chdir(path + currentDay)
-open(lastUpdated+".xml", 'w').write(data)
+path = os.getcwd()
+if 'data' not in os.listdir(path):
+	os.mkdir(path + '/data/')
+if currentDay not in os.listdir(path + '/data/'):
+	os.mkdir(path + '/data/' + currentDay)
+
+os.chdir(path + '/data/' + currentDay)
+open(lastUpdated + ".xml", 'w').write(data)
