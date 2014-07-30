2c0dbcafb40b1e37ec4cebfa69b6c3a07ad6289a
Logan Bernard <bernard.logan4@gmail.com>
2014-07-30 12:32:42 -0400

    name files by time after request; don't traverse XML

--- massDOT/store.py
+++ massDOT/store.py
@@ -1,17 +1,18 @@
 import urllib2, os, time, logging, datetime
-import xml.etree.cElementTree as ET
 
 """
 Pulls the xml file from massDOT's website for the specified data feed, 'url',
-and writes it to a new file, named by timestamp.
+and writes it to a new file, named by timestamp*.
 
 Creates a directory 'feedname' to house the feed's data. Inside feedname, a new directory
-is created each day and named by date to store each new file.
+is created each day and named by date to store versions of the feed.
 
 All new directories and files are stored in a directory 'data'
 inside the current working directory.
 
-** currently pulling timestamp from XML data - not response header 
+* the timestap used to name each file is recorded after
+the response is received; the update being written may
+not be consistent with the filename 
 """
 def feed(url, feedname):
 
@@ -19,11 +20,12 @@ def feed(url, feedname):
 
 	request = urllib2.Request(url)
 	data = urllib2.urlopen(request).read()
-	root = ET.fromstring(data)
-	lastUpdated = root[0].find('LastUpdated').text
-	currentDay = lastUpdated[:-13]
-
+	
 	resTime = (time.time() - start)
+
+	date = datetime.datetime.utcnow()
+	currentDay = ("%s-%s-%s" % (date.year, date.month, date.day))
+	lastUpdated = (currentDay + " %s:%s:%s UTC" % (date.hour, date.minute, date.second))
 
 	path = os.getcwd()
 	if 'data' not in os.listdir(path):
