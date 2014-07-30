d32aad626b0fcb451d450575185a4e1c912af1b5
Logan Bernard <bernard.logan4@gmail.com>
2014-07-30 14:23:44 -0400

    rm collectData.sh; read feeds from cfg file; write to single log file; edit docstring

--- massDOT/collectData.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-while true; do
-    python -c 'import store; store.feed("http://www.massdot.state.ma.us/feeds/traveltimes/RTTM_feed.aspx", "travelTimes")'
-    sleep 45
-done
--- massDOT/store.py
+++ massDOT/store.py
@@ -1,27 +1,34 @@
-import urllib2, os, time, logging, datetime
-
 """
-Pulls the xml file from massDOT's website for the specified data feed, 'url',
-and writes it to a new file, named by timestamp*.
+Stores MassDOT data feeds specified in 'feeds.ini'
+into a directory 'data' in the current working directory.
 
-Creates a directory 'feedname' to house the feed's data. Inside feedname, a new directory
-is created each day and named by date to store versions of the feed.
+Runs until user interrupt.
 
-All new directories and files are stored in a directory 'data'
-inside the current working directory.
-
-* the timestap used to name each file is recorded after
-the response is received; the update being written may
-not be consistent with the filename 
+Logging information specifying server response and file writing times
+is written to 'feedData.log' inside of the 'data' directory.
 """
-def feed(url, feedname):
+
+import urllib2, os, time, logging, datetime, ConfigParser
+
+def writeFeed(url, feedname):
+	"""
+	Pulls a specified data feed, 'url', from MassDOT's website
+	and writes the contents to a new file, named by timestamp*.
+
+	Creates a directory 'feedname' to house the feed's data. Inside 'feedname', a new
+	directory is created each day and named by date to store updates to the feed.
+
+	* the timestap used to name each file is recorded after
+	the response is received; the update being written may
+	not be consistent with the filename 
+	"""
 
 	start = time.time()
 
 	request = urllib2.Request(url)
 	data = urllib2.urlopen(request).read()
 	
-	resTime = (time.time() - start)
+	resTime = (time.time()-start)
 
 	date = datetime.datetime.utcnow()
 	currentDay = ("%s-%s-%s" % (date.year, date.month, date.day))
@@ -37,10 +44,19 @@ def feed(url, feedname):
 
 	os.chdir(path+'/data/'+feedname+'/'+currentDay)
 	open(lastUpdated+".xml",'w').write(data)
+	os.chdir(path)
 
 	writeTime = (time.time()-start-resTime) 
 
-	logging.basicConfig(filename=path+'/data/'+feedname+'/'+feedname+'.log',level=logging.INFO)
-	logging.info('Date/Time: '+lastUpdated)
+	logging.basicConfig(filename=path+'/data/feedData.log',level=logging.INFO)
+	logging.info('Data Feed: '+feedname+' Date/Time: '+lastUpdated)
 	logging.info('Response Time: '+`resTime`+' Write Time: '+`writeTime`)
 	logging.info('=========')
+
+
+config = ConfigParser.ConfigParser()
+config.read('feeds.ini')
+while True:
+	for feed in config.sections():
+		writeFeed(config.get(feed, 'url'), config.get(feed, 'name'))
+		time.sleep(1)
